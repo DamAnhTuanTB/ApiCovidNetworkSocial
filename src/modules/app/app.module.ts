85902261f2c23302c57af83037200b1823c76e4e
Ngọc Phạm <phamvanngoc0123@gmail.com>
2022-05-07 15:02:16 +0700

    Change password mysql local

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -17,7 +17,7 @@ import entities from 'src/typeorm';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: '04112000',
+      password: '',
       database: 'covid_network_social_db',
       entities,
       synchronize: true,
