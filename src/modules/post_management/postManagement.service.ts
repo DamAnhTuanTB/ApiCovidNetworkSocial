3ffa6fc780f5aed568d8a7e42dbb5280d1e4c5ed
Ngọc Phạm <phamvanngoc0123@gmail.com>
2022-05-19 23:28:54 +0700

    Fix bug notification

--- src/modules/post_management/postManagement.service.ts
+++ src/modules/post_management/postManagement.service.ts
@@ -400,8 +400,11 @@ export class PostManagementService {
     if (status == StatusPost.CANCEL) {
       const user = await this.userRepository.findOne({ id: idLogin });
       //Tạo thông báo
-      this.createNotification('Admin đã bình luận', user, postUpdate);
+      this.createNotification('Admin đã hủy bài viết', user, postUpdate);
     } else if (status == StatusPost.SUCCESS) {
+      const user = await this.userRepository.findOne({ id: idLogin });
+      //Tạo thông báo
+      this.createNotification('Admin đã phê duyệt bài viết', user, postUpdate);
     }
     return {
       statusCode: HttpStatus.OK,
